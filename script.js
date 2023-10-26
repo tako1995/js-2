e172a0e90837826485e9f47a9eb59e2f28646e30
tako1995 <141060940+tako1995@users.noreply.github.com>
2023-10-27 00:31:50 +0400

    js

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// დავალება 2
+
+let arry1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+
+
+
+// დავალება 3
+
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (let aa = 0; aa < array2.length; aa++) {
+//     const element = array2[aa];
+//     if (element) {
+        
+//     }
+    
+// }
+
+
+
+
+
+
+// დავალება 7
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active",
+//   };
+  
+//   console.log(user.studentstatus);
+
+
+  // დავალება 8
+
+
+//   let userInfo = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentstatus: 'active'
+// }
+
